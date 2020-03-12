4a907cfe1377de8ddbd442b5536503f66084a538
Jason Seegmiller <jasonseegmiller@overstock.com>
2020-03-11 19:36:03 -0600

    Code cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-  extends: [
-    'gsandf',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': ['off']
-      }
+    extends: [
+        'gsandf',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    overrides: [
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/explicit-function-return-type': ['off']
+            }
+        }
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'array-callback-return': 'error',
+        camelcase: 'off',
+        'no-extra-label': 'error',
+        'no-native-reassign': 'error',
+        'no-restricted-syntax': ['error'],
+        'no-unused-labels': 'error',
+        'no-useless-concat': 'error',
+        radix: ['error', 'as-needed'],
+        'require-yield': 'error',
+        'sort-keys': 'off'
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
     }
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'array-callback-return': 'error',
-    camelcase: 'off',
-    'no-extra-label': 'error',
-    'no-native-reassign': 'error',
-    'no-restricted-syntax': ['error'],
-    'no-unused-labels': 'error',
-    'no-useless-concat': 'error',
-    radix: ['error', 'as-needed'],
-    'require-yield': 'error',
-    'sort-keys': 'off'
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 import puppeteer from 'puppeteer';
 import { getSearchLinks, login, runSearch } from './ms-rewards';
 
-const isDev = process.env.NDOE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== 'production';
 
 async function main() {
-  const browser = await puppeteer.launch({
-    devtools: isDev
-  });
+    const browser = await puppeteer.launch({
+        devtools: isDev,
+        headless: !isDev,
+    });
 
-  const [_, searchLinks] = await Promise.all([
-    // Set the cookies necessary from logging in
-    login(browser),
-    // Get list of text to search for
-    getSearchLinks(browser)
-  ]);
+    const [_, searchLinks] = await Promise.all([
+        // Set the cookies necessary from logging in
+        login(browser),
+        // Get list of text to search for
+        getSearchLinks(browser),
+    ]);
 
-  // Create a list of searches to run, but don't run them yet
-  const runnableSearches = searchLinks.map(textContent => () =>
-    runSearch(browser, textContent)
-  );
+    // Create a list of searches to run, but don't run them yet
+    const runnableSearches = searchLinks.map((textContent: string) => () => runSearch(browser, textContent));
 
-  // Open searches in browser serially
-  for (const search of runnableSearches) {
-    await search();
-  }
+    // Open searches in browser serially
+    for (const search of runnableSearches) {
+        await search();
+    }
 
-  await browser.close();
+    await browser.close();
 }
 
 main();
